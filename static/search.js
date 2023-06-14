90c7737c73bd1993f5e08112717745238828ded8
Kwazy <124801774+KwazyMotoo@users.noreply.github.com>
2023-06-13 22:53:46 -0500

    Update search.js

--- static/search.js
+++ static/search.js
@@ -1,24 +1,33 @@
 "use strict";
 
+/**
+
+@param {string} input
+@param {string} template Template for a search query.
+@returns {string} Fully qualified URL
+*/
 function search(input, template) {
-const tryURL = (url) => {
+let url;
 try {
-return new URL(url).toString();
+url = new URL(input);
+// input is a valid URL:
+// eg: https://example.com, https://example.com/test?q=param
+return url.toString();
 } catch (err) {
-return null;
+// input was not a valid URL
 }
-};
-
-const inputURL = tryURL(input);
-if (inputURL) {
-return inputURL;
-}
-
-const inputWithHTTP = http://${input};
-const httpURL = tryURL(inputWithHTTP);
-if (httpURL && httpURL.includes(".")) {
-return httpURL;
+try {
+url = new URL(http://${input});
+// input is a valid URL when http:// is added to the start:
+// eg: example.com, https://example.com/test?q=param
+if (url.hostname.includes(".")) return url.toString();
+} catch (err) {
+// input was not a valid URL
 }
 
+// input may have been a valid URL, however the hostname was invalid
+
+// Attempts to convert the input to a fully qualified URL have failed
+// Treat the input as a search query
 return template.replace("%s", encodeURIComponent(input));
 }
